4abc8264293c1dbf7dbdb771e81c5e82d2c4cfff
Nolverto Urias Obeso <nolvertou@gmail.com>
2023-12-04 22:01:45 -0700

    Add binding to HomeFragment, observer to boolean val navigateToSearch and navigation

--- app/src/main/java/com/example/android/gdgfinder/home/HomeFragment.kt
+++ app/src/main/java/com/example/android/gdgfinder/home/HomeFragment.kt
@@ -5,8 +5,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import com.example.android.gdgfinder.R
+import com.example.android.gdgfinder.databinding.HomeFragmentBinding
 
 class HomeFragment : Fragment() {
 
@@ -20,9 +23,24 @@ class HomeFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout.home_fragment, container, false)
+        val binding = HomeFragmentBinding.inflate(inflater)
         viewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
 
-        return view
+        // Tell the binding object about our viewModel
+        binding.viewModel = viewModel
+
+        // Register an observer
+        viewModel.navigateToSearch.observe(viewLifecycleOwner, Observer<Boolean>{ navigate ->
+            if(navigate){
+                // Create navController object
+                val navController = findNavController()
+                // Navigate to gdgListFragment
+                navController.navigate(R.id.action_homeFragment_to_gdgListFragment)
+                // Restart the boolean value to falses
+                viewModel.onNavigatedToSearch()
+            }
+        })
+
+        return binding.root
     }
 }
